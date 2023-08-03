97fe7f3da949396fc96a2d60363d4f1cad9f6a48
PrashuCode97 <139844663+PrashuCode97@users.noreply.github.com>
2023-08-03 05:57:50 +0000

    solution

--- Stocklisting/src/main/java/com/stackroute/Stocklisting/controller/StockController.java
+++ Stocklisting/src/main/java/com/stackroute/Stocklisting/controller/StockController.java
@@ -20,4 +20,46 @@ public class StockController {
     // Autowired StockListingService class
     @Autowired
     StockListingService stockListingService;
+
+    // Method to get stocks by country name
+    @GetMapping("/stocks/{country}")
+    public ResponseEntity<?> getStocksByCountry(@PathVariable String country) {
+        // Call the getStocksByCountry method from StockListingService class by passing country name as parameter
+        // and store the response in List of Stock objects
+        Data stocksData = stockListingService.getStocksByCountry(country);
+        // Return the response as ResponseEntity
+        return new ResponseEntity<>(stocksData, HttpStatus.OK);
+    }
+
+    // Controller Method to get timeseries by symbol by making call to getStocksBySymbol method of StockListingService class
+    @GetMapping("/stocks/{symbol}/timeseries")
+    public ResponseEntity<?> getStocksBySymbol(@PathVariable String symbol) {
+        // Call the getStocksBySymbol method from StockListingService class by passing symbol as parameter
+        // and store the response in List of Stock objects
+        TimeSeries timeSeries = stockListingService.getStocksBySymbol(symbol);
+        // Return the response as ResponseEntity
+        return new ResponseEntity<>(timeSeries, HttpStatus.OK);
+    }
+
+    // Controller Method to post stock by making call to saveStock method of StockListingService class
+    // Taking stock details as Requestbody
+    @PostMapping("/stocks")
+    public ResponseEntity<?> saveStock(@RequestBody Stock stock) {
+        // Call the saveStock method from StockListingService class by passing stock object as parameter
+        // and store the response in Stock object
+        Stock stockResponse = stockListingService.saveStock(stock);
+        // Return the response as ResponseEntity
+        return new ResponseEntity<>(stockResponse, HttpStatus.OK);
+    }
+
+    // Controller Method to get all stocks by making call to getAllStocks method of StockListingService class
+    @GetMapping("/stocks")
+    public ResponseEntity<?> getAllStocks() {
+        // Call the getAllStocks method from StockListingService class
+        // and store the response in List of Stock objects
+        List<Stock> stocks = stockListingService.getAllStocks();
+        // Return the response as ResponseEntity
+        return new ResponseEntity<>(stocks, HttpStatus.OK);
+    }
+
 }
--- Stocklisting/src/main/java/com/stackroute/Stocklisting/service/StockListingService.java
+++ Stocklisting/src/main/java/com/stackroute/Stocklisting/service/StockListingService.java
@@ -17,4 +17,46 @@ public class StockListingService {
     // Spring Autowired StockRepo Interface
     @Autowired
     StockRepo stockRepo;
+
+    // Method to call https://api.twelvedata.com/stocks?country=${country} by passing country name as parameter
+    // and return a Data object
+    public Data getStocksByCountry(String country) {
+        // Create a RestTemplate object
+        RestTemplate restTemplate = new RestTemplate();
+        // Call the https://api.twelvedata.com/stocks?country=${country} by passing country name as parameter
+        // and store the response in Data object
+        Data stocksData = restTemplate.getForObject("https://api.twelvedata.com/stocks?country=" + country, Data.class);
+        // Return the response
+        return stocksData;
+    }
+
+    // Method to call https://api.twelvedata.com/time_series?symbol=${symbol}&interval=1min&apikey=0975893c35e04bc59419ad713796a678&outputsize=20 by passing symbol as parameter
+    // and return a TimeSeries object
+    public TimeSeries getStocksBySymbol(String symbol) {
+        // Create a RestTemplate object
+        RestTemplate restTemplate = new RestTemplate();
+        // Call the https://api.twelvedata.com/time_series?symbol=${symbol}&interval=1min&apikey=0975893c35e04bc59419ad713796a678&outputsize=20 by passing symbol as parameter
+        // and store the response in TimeSeries object
+        TimeSeries timeSeries = restTemplate.getForObject("https://api.twelvedata.com/time_series?symbol=" + symbol + "&interval=1min&apikey=0975893c35e04bc59419ad713796a678&outputsize=20", TimeSeries.class);
+        // Return the response
+        return timeSeries;
+    }
+
+    // Method to store stock details in database and return stock
+    public Stock saveStock(Stock stock) {
+        // Call the save method from StockRepo interface by passing stock object as parameter
+        // and store the response in Stock object
+        Stock stockResponse = stockRepo.save(stock);
+        // Return the response
+        return stockResponse;
+    }
+
+    // Method to get all stocks from database
+    public List<Stock> getAllStocks() {
+        // Call the findAll method from StockRepo interface
+        // and store the response in List of Stock objects
+        List<Stock> stocks = stockRepo.findAll();
+        // Return the response
+        return stocks;
+    }
 }
